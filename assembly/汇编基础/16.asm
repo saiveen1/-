8cf6a3ba4fe073e481787757da6aa4f8b62ae602
saiveen1 <34472475+saiveen1@users.noreply.github.com>
2020-01-22 23:43:02 +0800

    17

---
+++ assembly/汇编基础/16.asm
@@ -0,0 +1,35 @@
+assume cs:code
+
+a segment
+	db 1,2,3,4,5,6,7,8
+a ends
+
+b segment
+	db 1,2,3,4,5,6,7,8
+b ends
+
+d segment
+	db 0,0,0,0,0,0,0,0
+d ends
+
+code segment
+start:
+	mov ax,a
+	mov ds,ax
+	mov bx,0
+	
+	mov cx,8
+s:
+	mov al,ds:[bx]
+	add al,ds:[bx+16]
+	mov ds:[bx+32],al
+	inc bx
+	loop s
+	
+	mov ax,4c00h
+	int 21h
+	
+	
+code ends	
+
+end start
