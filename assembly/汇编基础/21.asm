a815c44c6de8a6dfd4a02a82a1b93a0ce6b8fadd
saiveen1 <34472475+saiveen1@users.noreply.github.com>
2020-01-23 02:55:42 +0800

    21

---
+++ assembly/汇编基础/21.asm
@@ -0,0 +1,42 @@
+assume cs:codesg,ds:datasg,ss:stacksg
+
+stacksg segment
+dw 0,0,0,0,0,0,0,0
+stacksg ends
+
+datasg segment
+	db '1.display       '
+	db '2.brows         '
+	db '3.replace       '
+	db '4.modify        '
+datasg ends
+
+codesg segment
+start:
+
+	mov ax,datasg
+	mov ds,ax
+
+	mov bx,2
+	mov si,0
+	mov cx,4
+
+s:
+	push cx
+	mov bx,2
+	s2:
+		mov al,ds:[si+bx]
+		and al,11011111b
+		mov [si+bx],al
+		inc bx
+		loop s2
+	
+	pop cx
+	mov cx,dx
+	add si,10h
+	loop s
+	
+	mov ax,4c00h
+	int 21h
+codesg ends
+end start
