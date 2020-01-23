7e35d2a00e6f560a90d795a7e02a7e82bd624014
saiveen1 <34472475+saiveen1@users.noreply.github.com>
2020-01-23 17:20:05 +0800

    22asm

---
+++ assembly/汇编基础/22.asm
@@ -0,0 +1,22 @@
+assume cs:codesg,ds:datasg
+
+datasg segment
+	dd 100001
+	dw 100
+	dw 0
+datasg ends
+
+codesg segment
+start:
+	
+	mov ax,datasg
+	mov ds,ax
+	mov ax,ds:[0]
+	mov dx,ds:[2]
+	div word ptr ds:[4]
+	
+	mov ds:[6],ax
+
+
+codesg ends
+end start
