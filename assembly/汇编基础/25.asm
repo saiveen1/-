7040e6277472aae5a0028b4ca3c33922a8ab4628
saiveen1 <34472475+saiveen1@users.noreply.github.com>
2020-01-26 23:39:24 +0800

    25asm

--- assembly/汇编基础/25.asm
+++ assembly/汇编基础/25.asm
@@ -5,7 +5,7 @@ data segment
 data ends
 
 stack segment
-	dw 2 dup(0)
+	dw 0,24h,71h
 stack ends
 
 code segment
@@ -18,18 +18,19 @@ start:
 	mov ax,stack
 	mov ss,ax
 	
-	mov si,780h
 	mov bx,0
-	mov di,0
 	mov cx,3h
-
+	mov dx,02h
+	mov sp,2
 s:
 	push cx
 	mov cx,10h
+	mov di,0
+	mov si,780h
 	
 	s2:  
 	  mov al,es:[di]  
-	  mov ah,71h
+	  mov ah,dl
 	  mov ds:[bx+si+40h],ax
 	  add si,2
 	  inc di
@@ -37,10 +38,11 @@ s:
 	
 	add bx,0a0h
 	pop cx
+	pop dx
 loop s
 	
 	mov ax,4c00h
 	int 21h
+	
 code ends
-
 end start
